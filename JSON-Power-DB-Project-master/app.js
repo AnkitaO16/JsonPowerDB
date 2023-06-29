4017052b794dc17ed767d416a76f6780455a1240
ANKITA OJHA <ankitaojha1611@gmail.com>
2023-06-29 23:35:16 +0530

    code update

---
+++ JSON-Power-DB-Project-master/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/home.html');
+})
+
+app.get('/getall', (req, res)=>{
+    res.sendFile(__dirname + '/public/getAll.html');
+})
+
+app.get('/getbykey', (req, res)=>{
+    res.sendFile(__dirname + '/public/getByKey.html');
+})
+
+app.get('/getbyrecord', (req, res)=>{
+    res.sendFile(__dirname + '/public/getByRecord.html');
+})
+
+app.get('/put', (req, res)=>{
+    res.sendFile(__dirname + '/public/put.html');
+})
+
+app.get('/update', (req, res)=>{
+    res.sendFile(__dirname + '/public/update.html');
+})
+
+app.get('/delete', (req, res)=>{
+    res.sendFile(__dirname + '/public/delete.html');
+})
+
+app.listen(port, ()=>{
+    console.log(`Port running at http://localhost:${port}`)
+});
+
